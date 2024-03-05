fd341e3cb5910fb1184629a50128044c31f4d8ae
Keiner <khuresc2805@g.educaand.es>
2024-03-05 19:24:37 +0100

    Agregados comentarios

--- Figura.java
+++ Figura.java
@@ -1,4 +1,14 @@
+/**
+*Representa la estructura básica de las figuras geométricas.
+*Las subclases deben implementar los métodos y otorgarle un cuerpo
+*
+*@autor Keiner
+*/
 public abstract class Figura {
+	/**
+	*Retornar el área de la figura
+	*@return ärea como doble
+	*/
     public abstract double area();
     public abstract double perimetro();
 }
